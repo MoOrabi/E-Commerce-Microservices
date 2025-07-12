d11586eacf5f87383fee8c1a9cbe66c5e957e513
MoOrabi <informatikerm@gmail.com>
2025-07-12 18:59:42 +0300

    Some progress at order ms

---
+++ services/order/src/main/java/com/moorabi/ecommerce/order/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.moorabi.ecommerce.order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+}
---
+++ services/order/src/main/java/com/moorabi/ecommerce/orderline/OrderLineRequest.java
@@ -0,0 +1,9 @@
+package com.moorabi.ecommerce.orderline;
+
+public record OrderLineRequest(
+        Integer id,
+        Integer orderId,
+        Integer productId,
+        double quantity
+) {
+}
